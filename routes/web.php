cef32889bca1b1288f37a0243095ba6aa52d2865
Klaudia Pabin <klaudiapabin75@gmail.com>
2026-06-14 16:30:18 +0200

    refactor dashboard route to use DashboardController for rendering

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'dashboard')->name('dashboard');
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::post('accounts', [AccountController::class, 'store'])->name('accounts.store');
 });
 
