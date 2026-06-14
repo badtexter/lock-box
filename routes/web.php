55f932f1ae3a0e1df5cfe3c02f9d915e553aa3f1
Klaudia Pabin <klaudiapabin75@gmail.com>
2026-06-14 16:12:51 +0200

    added route for account credential storage in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+    Route::post('accounts', [AccountController::class, 'store'])->name('accounts.store');
 });
 
 require __DIR__.'/settings.php';
