ebf2c8d97e48a1c371191a64cf4f7deddf1c1539
Klaudia Pabin <klaudiapabin75@gmail.com>
2026-06-14 16:12:33 +0200

    implemented store method in AccountController for credential validation and storage

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use Illuminate\Http\Request;
 
 class AccountController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'platform' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255'],
+            'password' => [
+                'required',
+                'string',
+                'min:14',
+                'regex:/[A-Z]/',
+                'regex:/[!@#$%^&*()_+\-=[\]{};\'":\\|,.<>\/?]/',
+            ],
+        ], [
+            'password.regex' => 'Hasło musi zawierać dużą literę i znak specjalny.',
+        ]);
+
+        Account::create($validated);
+
+        return back();
+    }
 }
